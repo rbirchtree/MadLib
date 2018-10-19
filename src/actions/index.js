3428944b05250f1141cf6012f92ccf368cfb8df2
Rob <rbirchtree@gmail.com>
2018-10-18 17:24:07 -0700

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+export const SET_NAME = 'SET_NAME';
+export const setName = name => ({
+	type: SET_NAME,
+	name
+});
+
+export const SET_AGE = 'SET_AGE';
+export const setAge = age => ({
+	type: SET_AGE,
+	age
+});
+
+export const SET_COLOR = 'SET_COLOR';
+export const setColor = color => ({
+	type: SET_COLOR,
+	color
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,26 @@
+import { SET_AGE, SET_COLOR, SET_NAME } from '../actions';
+
+const initalState = {
+	name: 'Bob',
+	age: 42,
+	color: 'blue'
+};
+
+export const madLibReducer = (state=initalState, action) => {
+	if (action.type === SET_AGE) {
+		return Object.assign({},state,{
+			age: action.age
+		});
+	}
+	else if (action.type === SET_COLOR){
+		return Object.assign({},state,{
+			color: action.color
+		});
+	}
+	else if(action.type === SET_NAME){
+		return Object.assign({}, state,{
+			name: action.name
+		});
+	}
+	return state;
+};
